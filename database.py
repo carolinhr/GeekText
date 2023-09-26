9d994b3245d31015b3692216957fb8ccba71c9da
Carolin <carolinhromero@gmail.com>
2023-09-26 18:35:15 -0400

    db commit

--- database.py
+++ database.py
@@ -4,15 +4,17 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from datetime import timedelta
 
 app = Flask(__name__)
+app.config['QLAlchemy_DATABASE_URI'] = 'sqlite:///users.sqlite3' #real URI TBD
 db = SQLAlchemy(app)
 
-Base = declarative_base()
-
-
-class User:
-    id int
-    username str
-    email str
-
+class users(db.Model):
+    _id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.Integer, primary_key = True) #need for having username as primary key TBD
+    password = db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    cc_number = db.Column('card number', db.String(16))
+    home_address = db.Column('home address', db.String)
+    # add more columns as needed 
 
 
