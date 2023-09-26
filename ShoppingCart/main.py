c53edc71235e0b97a8aea9fcaeb72e8c197bdaf6
voidplanet <kmrnharrison@gmail.com>
2023-09-25 22:47:27 -0400

    gitignore update & HelloWorld test

--- ShoppingCart/main.py
+++ ShoppingCart/main.py
@@ -2,7 +2,13 @@ from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api - Api(app)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
+
+api.add_resource(HelloWorld, "/helloworld")
 
 if __name__ == "__main__":
     app.run(debug=True)
