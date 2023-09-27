2f2841bc8f969e1229d9835254c583f97b7fb200
voidplanet <kmrnharrison@gmail.com>
2023-09-27 12:44:13 -0400

    dummy data for GET request

--- ShoppingCart/main.py
+++ ShoppingCart/main.py
@@ -4,14 +4,20 @@ from flask_restful import Api, Resource
 app =   Flask(__name__)
 api =   Api(app)
   
-class returnjson(Resource): # note to self: run main.py & open up the host address on browser before running API requests, otherwise there will be errors
+class ShoppingCart(Resource): # note to self: run main.py & open up the host address on browser before running API requests, otherwise there will be errors
     def get(self):
-        return {"data": "Hello World"}
+        return {"name": "Textbook1",
+                "price": "$60.00",
+                "description": "Textbook1 Description"
+                }
     
     def post(self):
-        return {"data": "Posted"}
+        return {"name": "Textbook2",
+                "price": "$70.00",
+                "description": "Textbook2 Description"
+                }
   
-api.add_resource(returnjson,'/returnjson')
+api.add_resource(ShoppingCart,'/shoppingcart')
   
   
 if __name__=='__main__':
--- ShoppingCart/test.py
+++ ShoppingCart/test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "returnjson")
+response = requests.get(BASE + "shoppingcart")
 print(response.json())
