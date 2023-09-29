e892dee981118a897d9449e9827a430cfc693700
Carolin <carolinhromero@gmail.com>
2023-09-28 20:01:44 -0400

    added User model to db

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-#new databse info pending
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
@@ -7,14 +6,22 @@ app = Flask(__name__)
 app.config['QLAlchemy_DATABASE_URI'] = 'sqlite:///users.sqlite3' #real URI TBD
 db = SQLAlchemy(app)
 
-class users(db.Model):
+class Users(db.Model):
     _id = db.Column(db.Integer, primary_key = True)
-    username = db.Column(db.Integer, primary_key = True) #need for having username as primary key TBD
-    password = db.Column(db.String)
+    username = db.Column(db.Integer, unique = True, nullable= False, primary_key = True) #need for having username as primary key TBD
+    password = db.Column(db.String, nullable= False)
     name = db.Column(db.String)
     email = db.Column(db.String)
-    cc_number = db.Column('card number', db.String(16))
+    cc_number = db.Column('card number', db.Integer(16))
     home_address = db.Column('home address', db.String)
-    # add more columns as needed 
+    
+    def __init__(self, username, password, name, email, cc_number, home_address):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
+        self.cc_number = cc_number
+        self.home_address = home_address
 
-
+#create new classes based on chosen features (ex: book genres, prices, etc.)
+db.create_all() #this should be placed after the last created db class 
