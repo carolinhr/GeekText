07ee222427e92c698bbef28228622d13c457196a
voidplanet <kmrnharrison@gmail.com>
2023-09-28 20:58:12 -0400

    GET request update

--- ShoppingCart/main.py
+++ ShoppingCart/main.py
@@ -5,19 +5,14 @@ app =   Flask(__name__)
 api =   Api(app)
 
 names = {"textbook": {"price": 70.00, "vol": 2},
-         "textbook2": {"price": 60.00, "vol": 1}}
+         "textbook2": {"price": 60.00, "vol": 1},
+         "textbook3": {"price": 65.00, "vol": 3}}
   
 class ShoppingCart(Resource): # note to self: run main.py in a separate split terminal before running requests in other files, otherwise there will be errors
-    def get(self, name, test):
-        return {"name": name, "test": test}
-    
-    def post(self):
-        return {"name": "Textbook2",
-                "price": "$70.00",
-                "description": "Textbook2 Description"
-                }
+    def get(self, name):
+        return names[name]
   
-api.add_resource(ShoppingCart,'/shoppingcart/<string:name>/<int:test>')
+api.add_resource(ShoppingCart,'/shoppingcart/<string:name>')
   
   
 if __name__=='__main__':
--- ShoppingCart/test.py
+++ ShoppingCart/test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "shoppingcart/textbook/5")
+response = requests.get(BASE + "shoppingcart/textbook2")
 print(response.json())
