f521b8b768cf5b139f6334be18159904abe2dd8a
carolinhr <114369011+carolinhr@users.noreply.github.com>
2023-10-12 22:11:53 -0400

    Update app.py

--- profilemgmt/app.py
+++ profilemgmt/app.py
@@ -7,39 +7,6 @@ api = Api(app)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Set to False to disable tracking
 db.init_app(app)
-
-#creating new user
-@app.route('/user', methods=['POST'])
-def add_user():
-    data = request.json #to access user fields in its json format
-    username = data['username']
-    password = data['password']
-    name = data['name']
-    email = data.get('email') # use .get() to handle missing keys 
-    cc_number = data.get('credit_card') 
-    home_address = data.get('home_address')
-
-    # Create a new User object and add it to the database
-    new_user = User(username, password, name, email, cc_number, home_address)
-    db.session.add(new_user)
-    db.session.commit()
-    return 'User added successfully', 201  # return a success response
-
-@app.route('/user/<username>', methods=['GET'])
-def get_user(username):
-    data = request.json #to access user fields in its json format
-    user = User.query.filter_by(username=username).first()
-    if user:
-        user_data = {
-            'username': user.username,
-            'name': user.name,
-            'email': user.email,
-            'home_address': user.home_address,
-            'password' : user.password
-        }
-        return jsonify(user_data), 200        
-    else:
-        return 'User not found', 404
     
 @app.route('/user/<username>', methods=['PATCH'])
 def update_user(username):
