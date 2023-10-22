c2e7e43f193a92f62cf59396c25236d0da13ed42
voidplanet <kmrnharrison@gmail.com>
2023-10-21 22:34:22 -0400

    Added user id params

--- ShoppingCart/app.py
+++ ShoppingCart/app.py
@@ -1,21 +1,18 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
   
 app =   Flask(__name__)
 api =   Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
-
-# class UserModel(db.Model):
-#     __tablename__ = 'user'
-#     id = db.Column(db.Integer, primary_key=True)
+migrate = Migrate(app, db)
 
 class TextbookModel(db.Model):
     __tablename__ = 'textbook'
     id = db.Column(db.Integer, primary_key=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # user = db.relationship("UserModel", backref=db.backref("user", uselist = False))
+    user_id = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
@@ -23,9 +20,9 @@ class TextbookModel(db.Model):
     def __repr__(self):
         return f"Textbook(name = {name}, author = {author}, price = {price})"
 
+db.create_all()
 
 textbook_post_args = reqparse.RequestParser()
-# textbook_post_args.add_argument("id", type=str, help="Textbook ID required", required = True)
 textbook_post_args.add_argument("name", type=str, help="Textbook name required", required = True)
 textbook_post_args.add_argument("price", type=float, help="Textbook price required", required = True)
 textbook_post_args.add_argument("author", type=str, help="Textbook author required", required = True)
@@ -33,7 +30,7 @@ textbook_post_args.add_argument("author", type=str, help="Textbook author requir
   
 resource_fields = {
     'id' : fields.Integer,
-    # 'user_id' : fields.Integer,
+    'user_id' : fields.Integer,
     'name' : fields.String,
     'author' : fields.String,
     'price' : fields.Float
@@ -41,39 +38,44 @@ resource_fields = {
 
 class ShoppingCart(Resource): # note to self: run main.py in a separate split terminal before running requests in other files, otherwise there will be errors
     @marshal_with(resource_fields)
-    def get(self, textbook_id):
-        result = TextbookModel.query.filter_by(id=textbook_id).first()
-        if not result:
-            abort(404, message="Textbook does not exist")
-        return result
+    def get(self, user_id):
+        textbooks = TextbookModel.query.filter_by(user_id=user_id).all()
+        if not textbooks:
+            abort(404, message="Shopping cart empty.")
+        textbook_list = [{"id": textbook.id, "name": textbook.name, "author": textbook.author, "price": textbook.price} for textbook in textbooks]
+        return textbook_list
     
     @marshal_with(resource_fields)
-    def post(self, textbook_id):
+    def post(self, user_id):
         args = textbook_post_args.parse_args()
-        result = TextbookModel.query.filter_by(id=textbook_id).first()
-        if result:
-            abort(409, message="Textbook ID already in use")
-
-        textbook = TextbookModel(id = textbook_id,
+        new_textbook = TextbookModel(user_id=user_id,
                                  name=args['name'],
                                  author=args['author'], 
                                  price=args['price'])
-        db.session.add(textbook)
+        db.session.add(new_textbook)
         db.session.commit()
-        return 'Textbook added successfully', 201
+        return {"message": "Textbook added successfully."}, 201
+  
+class Textbook(Resource):
+    @marshal_with(resource_fields)
+    def get(self, user_id, textbook_id):
+        textbook_to_get = TextbookModel.query.filter_by(id=textbook_id, user_id=user_id).first()
+        if not textbook_to_get:
+            abort(404, message="Textbook does not exist.")
+        return textbook_to_get
     
-    def delete(self, textbook_id):
-        result = TextbookModel.query.filter_by(id=textbook_id).first()
-        if not result:
-            abort(404, message="Textbook does not exist")
-        db.session.delete(result)
+    @marshal_with(resource_fields)
+    def delete(self, user_id, textbook_id):
+        textbook_to_delete = TextbookModel.query.filter_by(id=textbook_id, user_id=user_id).first()
+        if not textbook_to_delete:
+             abort(404, message="Textbook does not exist.")
+        db.session.delete(textbook_to_delete)
         db.session.commit()
-        return 'Textbook deleted successfully', 204
-  
-api.add_resource(ShoppingCart,'/shoppingcart/<int:textbook_id>')
-  
+        return {"message": f"Textbook with ID {textbook_id} has been deleted."}, 204
+        
+
+api.add_resource(ShoppingCart,'/shoppingcart/<int:user_id>')
+api.add_resource(Textbook,'/shoppingcart/<int:user_id>/<int:textbook_id>')
   
 if __name__=='__main__':
     app.run(debug=True)
-
-
--- ShoppingCart/test.py
+++ ShoppingCart/test.py
@@ -6,12 +6,12 @@ data = [{"name" : "textbook0", "author" : "author0", "price" : 60.00},
         {"name" : "textbook1", "author" : "author1", "price" : 65.00},
         {"name" : "textbook2", "author" : "author2", "price" : 70.00}]
 
-for i in range(len(data)):
-    response = requests.post(BASE + "shoppingcart/" + str(i), data[i])
-    print(response)
+# for i in range(len(data)):
+#     response = requests.post(BASE + "shoppingcart/0", data[i])
+#     print(response)
 
 # input()
-# response = requests.delete(BASE + "shoppingcart/0")
+# response = requests.delete(BASE + "shoppingcart/0/1")
 # print(response)
 input()
 response = requests.get(BASE + "shoppingcart/0")
